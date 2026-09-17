60f063c37a165b6264d64176112aae9929891c8d
JhonyOwner <joao.vgbrisola@icloud.com>
2026-09-17 18:58:53 -0300

    aa

--- TESTES/calc.test.js
+++ TESTES/calc.test.js
@@ -133,6 +133,9 @@ const parcelaNoPiso = telaCalc({ ...activeInput, dinheiro: 10000, formaRestante:
 checar(parcelaNoPiso.parcelaPos >= parcelaNoPiso.parcelaMinima, 'abatimento na parcela deve respeitar o piso');
 checar(parcelaComLance.parcelaPos < prazoComLance.parcelaPos, 'abatimento na parcela deve custar menos que abatimento no prazo');
 checar(prazoComLance.mesesPosContemplacao < prazoComLance.mesesRestantesContrato, 'abatimento no prazo deve reduzir meses');
+const primeiraContemplacao = telaCalc({ ...activeInput, mesContemplacao: 1, adesaoPct: 2, formaRestante: 'prazo' });
+checar(primeiraContemplacao.primeiraParcela > primeiraContemplacao.parcelaReduzida, 'a primeira parcela deve incluir a adesão');
+checar(primeiraContemplacao.valorParcelasAntesContemplacao === primeiraContemplacao.primeiraParcela, 'a primeira parcela deve ser paga na contemplação do mês 1');
 
 if (falhas > 0) {
   console.error(`\n${falhas} falha(s) encontrada(s).`);
